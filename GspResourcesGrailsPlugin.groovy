06b34bf37d6baea5f9646bd13d32a361c3a2ff6c
Francis McKenzie <mail@hkwebentrepreneurs.com>
2012-03-18 22:47:19 +0100

    Upgraded to v0.3. Added delayable resources processing. Added new gspResourcePageRenderer and gspResourceLocator spring-injected beans. Added me (Francis) as plugin author and updated scm link to mckenfra repo.

--- GspResourcesGrailsPlugin.groovy
+++ GspResourcesGrailsPlugin.groovy
@@ -1,21 +1,61 @@
+import org.grails.plugin.resource.gsp.GspResourceLocator
+import org.grails.plugin.resource.gsp.GspResourcePageRenderer
+import org.grails.plugin.resource.override.DelayableResourceProcessor
+
 class GspResourcesGrailsPlugin {
-    def version = "0.2.1"
-    def grailsVersion = "1.3.7 > *"
-    def dependsOn = [resources: '1.0.2 > *']
+    def version = "0.3"
+    def grailsVersion = "2.0.0 > *"
+    def dependsOn = [
+        'resources': '1.1.5 > *',
+        'servlets': '2.0.0 > *',
+        'groovyPages': '2.0.0 > *'
+    ]
     def pluginExcludes = [
-            'src/groovy/**/*',
+            'src/groovy/**/test/*',
             'grails-app/views/*.gsp',
             'grails-app/controllers/**/*',
             'web-app/**/*',
             '**/MyAppResources.groovy',
     ]
 
-    def author = "Stefan Kendall"
-    def authorEmail = "stefankendall@gmail.com"
+    def author = "Stefan Kendall, Francis McKenzie"
+    def authorEmail = "stefankendall@gmail.com, francis.mckenzie@gmail.com"
     def title = "GSP Resources"
-    def description = 'Use the resources plugin to include static files like main.css.gsp, so dynamically built CSS and JS can be served as proper files instead of inlined in a non-cacheable GSP file. Note that changes to the GSP will cause a recompile, but changes to GSP data will not.'
+    def description = 'Use resources plugin to serve dynamically-built CSS and JS as static files instead of dynamic (non-cacheable) GSPs.'
 
     def documentation = "http://grails.org/plugin/gsp-resources"
 
-    def scm = [url: "https://github.com/stefankendall/gsp-resources"]
+    def scm = [url: "https://github.com/mckenfra/gsp-resources"]
+	
+    // Inject the renderer, gsp locator, and override the resources plugin processor
+	def doWithSpring = {
+		gspResourcePageRenderer(GspResourcePageRenderer, ref("groovyPagesTemplateEngine")) { bean ->
+			bean.lazyInit = true
+			groovyPageLocator = groovyPageLocator
+			grailsResourceLocator = grailsResourceLocator
+            grailsApplication = application
+		}
+        
+        gspResourceLocator(GspResourceLocator) { bean ->
+            bean.lazyInit = true
+            groovyPageLocator = groovyPageLocator
+            grailsResourceLocator = grailsResourceLocator
+        }
+        
+        // Override processor in resources plugin
+        grailsResourceProcessor(DelayableResourceProcessor) {
+            grailsLinkGenerator = ref('grailsLinkGenerator')
+            if (springConfig.containsBean('grailsResourceLocator')) {
+                grailsResourceLocator = ref('grailsResourceLocator')
+            }
+            grailsApplication = ref('grailsApplication')
+        }
+	}
+    
+    // Start the resources processing if necessary - otherwise, may be done during bootstrap
+    def doWithDynamicMethods = { appCtx ->
+        if ( appCtx.grailsResourceProcessor.isStartOnPluginLoad() ) {
+            appCtx.grailsResourceProcessor.start()
+        }
+    }
 }
