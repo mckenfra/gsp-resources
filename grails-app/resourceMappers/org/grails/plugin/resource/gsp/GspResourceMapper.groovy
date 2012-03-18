8432acf5ad1823c31a5b86d90cdabcef8b51b777
Francis McKenzie <mail@hkwebentrepreneurs.com>
2012-03-18 22:24:22 +0100

    Moved to new package structure. Refactored to move GSP compilation to GspResourceMeta - i.e. a synthetic module. Benefit is that compilation is deferred until after static resources have been processed. Also, lookup of GSP files changed to using GspResourceLocator. Benefit is that it can seemlessly find GSP resource files in plugins.

---
+++ grails-app/resourceMappers/org/grails/plugin/resource/gsp/GspResourceMapper.groovy
@@ -0,0 +1,84 @@
+package org.grails.plugin.resource.gsp
+
+import org.apache.commons.logging.LogFactory
+import org.grails.plugin.resource.mapper.MapperPhase
+import org.grails.plugin.resource.gsp.GspResourceLocator
+import org.grails.plugin.resource.gsp.GspResourceMeta
+import org.grails.plugin.resource.gsp.GspResourcePageRenderer
+
+/**
+ * A mapper for compiling GSP resource files.
+ * <p>
+ * <b>Notes</b>
+ * <p>
+ * Processing of GSP resources must be deferred until after other synthetic resources
+ * have been processed. This is because a GSP may contain <r:require /> tags, which will only
+ * render correctly when the resources plugin has finished preparing the relevant resources.
+ * <p>
+ * So the compilation of the GSP has to be delayed to the synthetic-resource processing phase,
+ * rather than the non-synthetic-resource processing phase. This is achieved by setting the
+ * initial GSP resource to be a 'delegating' resource, delegating to a target synthetic
+ * resource that has the desired post-compile URI (i.e. with .gsp suffix removed).
+ * The actual GSP compilation is then done when the synthetic resource is processed.
+ * <p>
+ * Note that if your GSP contains <r:require /> tags, you will have to explicitly
+ * list these in the 'dependsOn' setting for the module that contains the GSP in your
+ * Resoures.groovy config file.</li>
+ *
+ * @author Stefan Kendell
+ * @author Francis McKenzie
+ */
+class GspResourceMapper {
+    def log = LogFactory.getLog(this.class)
+    
+    def phase = MapperPhase.GENERATION
+
+    def priority = -1
+
+    static defaultExcludes = ['**/*.js', '**/*.png', '**/*.gif', '**/*.jpg', '**/*.jpeg', '**/*.gz', '**/*.zip']
+    static defaultIncludes = ['**/*.gsp']
+
+    // Injected
+    GspResourceLocator gspResourceLocator
+    GspResourcePageRenderer gspResourcePageRenderer
+    def grailsResourceProcessor
+    
+    def map(resource, config) {
+        if (log.isDebugEnabled()) {
+            log.debug "Checking if is a GSP: ${resource.sourceUrl}"
+        }
+        
+        // Check if GSP
+        Map gsp = gspResourceLocator.findGsp(resource.sourceUrl)
+        if (gsp) {
+            
+            // Generate the url of the compiled resource
+            String actualUrl = gspResourceLocator.generateCompiledFilenameFromOriginal(resource.originalUrl)
+
+            // Now create the synthetic resource that this resource is going to delegate to
+            def gspResource = grailsResourceProcessor.findSyntheticResourceById(actualUrl)
+            if (! gspResource) {
+                // Creates a new resource and empty file
+                gspResource = grailsResourceProcessor.newSyntheticResource(actualUrl, GspResourceMeta)
+                gspResource.id = actualUrl
+                gspResource.contentType = resource.contentType
+                gspResource.disposition = resource.disposition
+                gspResource.gsp = gsp
+                gspResource.gspResourceLocator = gspResourceLocator
+                gspResource.gspResourcePageRenderer = gspResourcePageRenderer
+                if (log.isDebugEnabled()) {
+                    log.debug "Created synthetic GSP resource: ${actualUrl}"
+                }
+            } else {
+                if (log.isDebugEnabled()) {
+                    log.debug "Synthetic GSP resource already exists: ${actualUrl}"
+                }
+            }
+            resource.delegateTo(gspResource)
+        } else {
+            if (log.isDebugEnabled()) {
+                log.debug "Not a GSP: ${resource.sourceUrl}"
+            }
+        }
+    }
+}
