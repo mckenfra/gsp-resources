a36536b3c84591335bc659db8f81ddb1a144f127
Francis McKenzie <mail@hkwebentrepreneurs.com>
2012-03-20 22:11:58 +0100

    Removed dependency on spring-test

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,6 +2,7 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
+
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -24,7 +25,6 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
-        runtime 'org.springframework:spring-test:3.0.6.RELEASE'
     }
     plugins {
         build(":release:latest.integration") {
