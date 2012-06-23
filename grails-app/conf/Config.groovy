0b44ada412a3959aebd18807e8a0960ad669cf5c
Francis McKenzie <mail@hkwebentrepreneurs.com>
2012-06-24 00:48:15 +0800

    Enable external links in API docs

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -27,3 +27,10 @@ log4j = {
 
 grails.views.default.codec="none" // none, html, base64
 grails.views.gsp.encoding="UTF-8"
+
+// For API docs
+grails.doc.api.org.grails.plugin.resource = "http://grails-plugins.github.com/grails-resources/gapi"
+grails.doc.api.javax.servlet = "http://docs.oracle.com/javaee/6/api"
+grails.doc.api.java.lang = "http://docs.oracle.com/javase/6/docs/api"
+grails.doc.api.java.util = "http://docs.oracle.com/javase/6/docs/api"
+grails.doc.api.groovy = "http://groovy.codehaus.org/gapi"
