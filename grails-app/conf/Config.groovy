2671006fe687292ba7638953c86e01b4e5b53712
Francis McKenzie <mail@hkwebentrepreneurs.com>
2012-06-25 00:44:42 +0800

    Added java.io API docs link

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -30,6 +30,7 @@ grails.views.gsp.encoding="UTF-8"
 
 // For API docs
 grails.doc.api.groovy = "http://groovy.codehaus.org/gapi"
+grails.doc.api.java.io = "http://docs.oracle.com/javase/6/docs/api"
 grails.doc.api.java.lang = "http://docs.oracle.com/javase/6/docs/api"
 grails.doc.api.java.util = "http://docs.oracle.com/javase/6/docs/api"
 grails.doc.api.javax.servlet = "http://docs.oracle.com/javaee/6/api"
