a8cbb065ce27fde946cc1c9f487691bc0d1e0b2e
Burt Beckwith <burt@burtbeckwith.com>
2013-09-13 03:34:53 -0400

    don't use dependsOn in 2.0+, use dependencies in BuildConfig.groovy

--- GspResourcesGrailsPlugin.groovy
+++ GspResourcesGrailsPlugin.groovy
@@ -5,11 +5,6 @@ import org.grails.plugin.gspresources.GspResourceProcessor
 class GspResourcesGrailsPlugin {
     def version = "0.4.2"
     def grailsVersion = "2.0.0 > *"
-    def dependsOn = [
-        'resources': '1.1.5 > *',
-        'servlets': '2.0.0 > *',
-        'groovyPages': '2.0.0 > *'
-    ]
     def pluginExcludes = [
             'src/groovy/**/test/*',
             'grails-app/views/**/*',
